089e62b50422552a2f65a46457430ff4611781b2
Marc Görtz <mail@marcgoertz.de>
2019-04-11 22:49:56 +0200

    Improve test coverage.

--- src/hamburger-button/hamburger-button.test.jsx
+++ src/hamburger-button/hamburger-button.test.jsx
@@ -5,27 +5,24 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { mount } from 'enzyme';
+import { render } from 'react-testing-library';
+import userEvent from 'user-event';
 import HamburgerButton from '.';
 
 describe('<HamburgerButton />', () => {
   it('renders a hamburger button', () => {
-    const component = renderer.create(
+    const container = renderer.create(
       <HamburgerButton position={{ top: '1em', left: '1em' }} />
     );
-    expect(component.toJSON()).toMatchSnapshot();
+    expect(container.toJSON()).toMatchSnapshot();
   });
   it('toggles the hamburger button on change', () => {
-    const mockFn = jest.fn();
-    const component = mount(
-      <HamburgerButton
-        position={{ bottom: '1em', right: '1em' }}
-        onChange={mockFn()}
-      />
+    const { container } = render(
+      <HamburgerButton position={{ bottom: '1em', right: '1em' }} />
     );
-    expect(component.state('checked')).toBe(false);
-    component.find('input').simulate('change');
-    expect(component.state('checked')).toBe(true);
-    expect(mockFn).toHaveBeenCalledTimes(1);
+    expect(container).toMatchSnapshot();
+    const input = container.querySelector('input');
+    userEvent.click(input);
+    expect(container).toMatchSnapshot();
   });
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,1 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import 'jest-styled-components';
-
-configure({
-  adapter: new Adapter(),
-});
