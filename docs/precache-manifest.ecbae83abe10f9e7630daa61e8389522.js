202d4563bfd1961f832fd023d1fa38c8c8a53f35
Marc Görtz <mail@marcgoertz.de>
2019-06-25 23:16:48 +0200

    Update Storybook.

---
+++ docs/precache-manifest.ecbae83abe10f9e7630daa61e8389522.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6604a3ab6c36d00220d09c2c233dc31a",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.2ea9ac2bdaff2d2132a8.bundle.js"
+  },
+  {
+    "url": "runtime~main.2ea9ac2bdaff2d2132a8.bundle.js"
+  },
+  {
+    "url": "vendors~main.2ea9ac2bdaff2d2132a8.bundle.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.7ffe77f1297032e8b919e35d8f216f98.js"
+  "precache-manifest.ecbae83abe10f9e7630daa61e8389522.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
