fe24c09ea5c45a5849a924c81d2c2e1efcb99de2
Marc Görtz <mail@marcgoertz.de>
2019-09-17 00:45:18 +0200

    Update Storybook build.

--- docs/main.46b2e7a3ff032f0c9422.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{387:function(n,o,p){p(388),p(490),n.exports=p(833)},490:function(n,o,p){"use strict";p.r(o);p(491),p(810),p(830)}},[[387,1,2]]]);
