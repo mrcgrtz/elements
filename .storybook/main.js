3e2385ef76441f8b162d1f1b0d08b01ba84e6fb7
Marc Görtz <mail@marcgoertz.de>
2020-02-04 12:59:43 +0100

    Update Storybook configuration.

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.(jsx|mdx)'],
+  addons: [
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-links/register',
+  ],
+};
