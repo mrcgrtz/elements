49cd1fb0a50299e6126250c7b69e627c5c5084db
Marc Görtz <mail@marcgoertz.de>
2022-09-20 15:50:23 +0200

    Update Storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,11 @@
+/** @type import('@storybook/core-common').StorybookConfig */
 module.exports = {
   stories: ['../src/**/*.stories.@(tsx|mdx)'],
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-a11y/register',
   ],
+  features: {
+    postcss: false,
+  }
 };
