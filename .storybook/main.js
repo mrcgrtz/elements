5938715a5dea73a6759cec29f7dec649d1dfe7ca
Marc Görtz <mail@marcgoertz.de>
2023-04-24 21:48:47 +0200

    Upgrade Storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,13 @@
 /** @type {import('@storybook/core-common').StorybookConfig} */
 module.exports = {
   stories: ['../src/**/*.stories.@(tsx|mdx)'],
-  core: {
-    builder: 'webpack5',
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
   },
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
   features: {
     postcss: false,
   },
+  disableTelemetry: true,
 };
