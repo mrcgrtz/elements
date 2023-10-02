f64e3eeca0c9ae8bd47fa733315cbb8aef4177cd
Marc Görtz <mail@marcgoertz.de>
2023-10-02 16:35:23 +0200

    Fix linter issues.

--- src/hamburger-button/tests/index.test.tsx
+++ src/hamburger-button/tests/index.test.tsx
@@ -5,7 +5,7 @@
 
 import React from 'react';
 import {render} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import {userEvent} from '@testing-library/user-event';
 import HamburgerButton from '..';
 
 describe('<HamburgerButton />', () => {
