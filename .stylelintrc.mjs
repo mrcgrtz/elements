230ad69b29e4e660fd26355fce6b96fde3dc2ddd
Marc Görtz <mail@marcgoertz.de>
2025-02-07 17:59:26 +0100

    Migrate Stylelint configuration to ESM.

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -1,9 +1,5 @@
 /** @type {import('stylelint').Config} */
-module.exports = {
+export default {
   customSyntax: 'postcss-styled-syntax',
   extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
-  rules: {
-    // disabled as this messes up template string interpolation in Styled Components
-    'comment-empty-line-before': null,
-  },
 };
